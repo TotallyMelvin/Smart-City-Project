0203998b04c7bdb13e77ed0bcae389c0db0580f5
IFB299-Group16 <bobbybristlenose@gmail.com>
2017-10-08 19:50:04 +1000

    Fixed up Bryce's merges.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/home'
+
+#Email Backend
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend' 
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False 
+EMAIL_PORT = 1025
