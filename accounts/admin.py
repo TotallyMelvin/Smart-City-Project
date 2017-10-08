2b7c52395deb10569c56bcb9192e49b9ca2a57df
Jamiekost <30399268+Jamiekost@users.noreply.github.com>
2017-10-08 18:03:27 +1000

    no message

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounts.models import UserProfileModel
+from accounts.models import UserProfileModel, userTypeAccessModel, FeatureLocationModel
 # Register your models here.
 
 admin.site.register(UserProfileModel)
